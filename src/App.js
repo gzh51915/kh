5a3d441388d391e9147bdab3cf9591f953399be5
tony_ <413466311@qq.com>
2020-06-16 19:45:58 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, BrowserRouter, withRouter, Route, Redirect, Switch, Link, NavLink } from 'react-router-dom';
+import Home from './pages/Home';
+// import Reg from './pages/Reg';
+// import Login from './pages/Login';
+
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <Home/>
+      {/* <Reg/>
+      <Login/> */}
+      
     </div>
+   
   );
 }
+// App = withRouter(App);
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React,{Component,PureComponent} from 'react'
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+              Home
+            </div>
+        )
+    }
+}
+
+
+export default Home;
